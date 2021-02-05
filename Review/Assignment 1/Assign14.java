1dc5d004a4286fe408db79f7d9d5a40e2c44f54a
Simon Chase <smchase@protonmail.com>
2021-02-04 22:19:55 -0500

    done assign14

---
+++ Review/Assignment 1/Assign14.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Assign14 {
+	public static void main(String[] args) {
+		String bignum = "";
+		int num, total = 0;
+		boolean[] used = new boolean[30];
+
+		for (int i = 0; i < 10; i++) {
+			do {
+				num = 1+(int)(Math.random()*30);
+			} while (used[num-1]);
+			used[num-1] = true;
+
+			System.out.print(num + (i<9 ? ", " : ""));
+			bignum += num;
+		}
+
+		System.out.printf("\n%s\n", bignum);
+
+		for (int i = 0; i < bignum.length(); i += 2) {
+			System.out.print(bignum.charAt(i) + (i<bignum.length()-2 ? " + " : " = "));
+			total += Character.getNumericValue(bignum.charAt(i));
+		}
+		System.out.println(total);
+	}
+}
